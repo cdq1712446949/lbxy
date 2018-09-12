979a37a32b8a6598be015e1853f3766f9129be36
CaoDongquan <1712446949@qq.com>
2018-09-12 10:25:42 +0800

    9.12

--- src/main/java/com/lbxy/common/status/ManagerStatus.java
+++ src/main/java/com/lbxy/common/status/ManagerStatus.java
@@ -1,5 +1,11 @@
 package com.lbxy.common.status;
 
+/**
+ * @author lmy
+ * @description ManagerStatus
+ * @date 2018/9/12
+ */
+
 public class ManagerStatus {
 
     public static final int SUPER_MANAGER=0;//0表示超级管理员
