47283e395f011c705ad10eb20f63495941c83f89
CaoDongquan <1712446949@qq.com>
2018-09-15 00:03:43 +0800

    9.13

---
+++ src/main/java/com/lbxy/core/interceptors/RoleInterceptor.java
@@ -0,0 +1,10 @@
+package com.lbxy.core.interceptors;
+
+import com.jfinal.aop.Invocation;
+
+public class RoleInterceptor implements com.jfinal.aop.Interceptor {
+    @Override
+    public void intercept(Invocation invocation) {
+
+    }
+}
