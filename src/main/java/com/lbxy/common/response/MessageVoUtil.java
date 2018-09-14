4f7aec1237337fb285c21d169730baefe341e1a9
laumgjyu <1176424359@qq.com>
2018-09-14 18:53:22 +0800

    修改messageUtil bug

--- src/main/java/com/lbxy/common/response/MessageVoUtil.java
+++ src/main/java/com/lbxy/common/response/MessageVoUtil.java
@@ -2,15 +2,15 @@ package com.lbxy.common.response;
 
 public class MessageVoUtil {
     public static MessageVo success(String message, Object data) {
-        return new MessageVo().setData(ResponseStatus.success).setMessage(message).setData(data);
+        return new MessageVo().setStatus(ResponseStatus.success).setMessage(message).setData(data);
     }
 
     public static MessageVo success(Object data) {
-        return new MessageVo().setData(ResponseStatus.success).setMessage("处理成功").setData(data);
+        return new MessageVo().setStatus(ResponseStatus.success).setMessage("处理成功").setData(data);
     }
 
     public static MessageVo success(String message) {
-        return new MessageVo().setMessage(message);
+        return new MessageVo().setStatus(ResponseStatus.success).setMessage(message);
     }
 
     public static MessageVo error(String msg) {
